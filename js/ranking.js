229a2d7c07127d5d58a62a213ec0e4d2041bdd0f
Alice Cheng <alice@huntsman-ve501-1014.apn.wlan.upenn.edu>
2018-04-15 17:55:38 -0400

    Added html/js basic structure (still with placeholders)

--- js/ranking.js
+++ js/ranking.js
@@ -1,45 +1,10 @@
-var app = angular.module('Friendship',[]);
+var app = angular.module('Ranking',[]);
 
-app.controller('friendshipController', function($scope, $http) {
-               var request = $http.get('/friendships_table');
-               request.success(function(data) {
-                               $scope.data = data;
-                               });
-               request.error(function(data){
-                             console.log('err');
-                             });
-               });
-/*
-app.controller('familyController', function($scope, $http) {
-                               $scope.data = "";
+app.controller('rankingController', function($scope, $http) {
+  var url = '/show_tables';
+  var request = $http.get(url);
+  request.success(function(data) {
+    $scope.data = data;
+  });
+               
 });
-
-function changeFamilyController() {
-  var val = document.getElementById("searchforfamily").value;
-  var url = '/findfamily/' + '\'' + val + '\'';
-  alert(url)
-  app.controller('familyController', function($scope, $http) {
-               var request = $http.get(url);
-               request.success(function(data) {
-                               $scope.data = data;
-                               });
-               request.error(function(data){
-                             console.log('err');
-                             });
-               });
- 
-}*/
-
-app.controller('familyController', function($scope, $http) {
-  $scope.data = "";
-  $scope.changeFamilyController = function () {
-
-    var val = document.getElementById("searchforfamily").value;
-    var url = '/findfamily/' + '\'' + val + '\'';
-    var request = $http.get(url);
-    request.success(function(data) {
-      $scope.data = data;
-    });
-  }
-});
-
