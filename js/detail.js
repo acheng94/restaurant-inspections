4b2ee121e493c59066e3809a5a5b9e603e29abaa
Alice Cheng <alice@huntsman-ve501-1014.apn.wlan.upenn.edu>
2018-04-15 19:06:02 -0400

    DAddedbasic table printing

--- js/detail.js
+++ js/detail.js
@@ -1,10 +1,15 @@
 var app = angular.module('Detail',[]);
 
 app.controller('detailController', function($scope, $http) {
-  var url = '/show_tables';
-  var request = $http.get(url);
-  request.success(function(data) {
-    $scope.data = data;
-  });
+  $scope.data = "";
+  $scope.changeDetailController = function () {
+
+    var val = document.getElementById("searchid").value;
+    var url = '/sql/' + '* FROM business WHERE id=' + '\'' + val + '\'';
+    var request = $http.get(url);
+    request.success(function(data) {
+      $scope.data = data;
+    });
+  }
                
 });
