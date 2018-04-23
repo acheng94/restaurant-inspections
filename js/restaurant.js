5287fdcfbf2fa45afc81c4a0e41df45732ccb66b
Sourya Roy <souryaroy@u.northwestern.edu>
2018-04-23 00:43:35 -0400

    Pretty restaurant search. Changed home menus.

--- js/restaurant.js
+++ js/restaurant.js
@@ -1,10 +1,12 @@
 var app = angular.module('Restaurant',[]);
 
 app.controller('restaurantController', function($scope, $http) {
+  var restdisp = document.getElementById("restcontainer");
   $scope.loadData = function () {
         var request = $http.get("/get_restaurants/" + $scope.searchName);
         request.success(function(data) {
             $scope.data = data;
+            restdisp.style.display = $scope.data == "" ? "none" : "block";
         });
         request.error(function(data){
             $scope.data = '';
